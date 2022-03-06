81038fcbe2523a31f629330a4e9dd8ed087a91c9
eythan <ecarter202@gmail.com>
2022-03-06 13:45:32 -0700

    update

--- sms.go
+++ sms.go
@@ -3,37 +3,43 @@ package sms
 import (
 	"errors"
 	"fmt"
-	"gopkg.in/yaml.v1"
 	"io/ioutil"
 	"net/smtp"
 	"strings"
+
+	"github.com/BurntSushi/toml"
 )
 
-type SMSClient struct {
-	Address    string
-	Port       int
-	Username   string
-	Password   string
-	config     *Config
-	smtpClient *smtp.Client
+type Client struct {
+	OutboundServer string
+	Port           int
+	EmailAddress   string
+	Password       string
+	config         *Config
+	smtpClient     *smtp.Client
 }
 
 type Config struct {
-	FromAddress string            `yaml:"from_address"`
-	Carriers    map[string]string `yaml:"carriers"`
+	FromAddress string            `toml:"from_address"`
+	Carriers    map[string]string `toml:"carriers"`
 }
 
-func createClient(address string, port int, username string, password string) (SMSClient, error) {
-	client := SMSClient{Address: address, Port: port, Username: username, Password: password}
+func New(outboundServer, email, password string, port int) (*Client, error) {
+	client := &Client{
+		OutboundServer: outboundServer,
+		EmailAddress:   email,
+		Password:       password,
+		Port:           port,
+	}
 	config := Config{Carriers: make(map[string]string)}
-	buffer, err := ioutil.ReadFile("sms.yml")
 
+	buffer, err := ioutil.ReadFile("sms.toml")
 	if err != nil {
-		return SMSClient{}, err
+		return nil, fmt.Errorf("reading toml file: %v", err)
 	}
 
-	if err := yaml.Unmarshal(buffer, &config); err != nil {
-		return SMSClient{}, err
+	if err := toml.Unmarshal(buffer, &config); err != nil {
+		return nil, fmt.Errorf("unmarshaling toml file: %v", err)
 	}
 
 	client.config = &config
@@ -41,18 +47,18 @@ func createClient(address string, port int, username string, password string) (S
 	return client, nil
 }
 
-func (client *SMSClient) Deliver(number, carrier, message string) error {
+func (client *Client) Deliver(number, carrier, message string) error {
 	c := client.config.Carriers[strings.ToLower(carrier)]
 
 	if c == "" {
-		return errors.New("Unsupported carrier. Please check sms.yml for supported carriers.")
+		return errors.New("Unsupported carrier. Please check sms.toml for supported carriers.")
 	}
 
 	to := []string{fmt.Sprintf("%s%s", number, c)}
-	auth := smtp.PlainAuth("", client.Username, client.Password, client.Address)
-	if err := smtp.SendMail(fmt.Sprintf("%s:%d", client.Address, client.Port), auth, client.config.FromAddress,
+	auth := smtp.PlainAuth("", client.EmailAddress, client.Password, client.OutboundServer)
+	if err := smtp.SendMail(fmt.Sprintf("%s:%d", client.OutboundServer, client.Port), auth, client.config.FromAddress,
 		to, []byte(message)); err != nil {
-		return err
+		return fmt.Errorf("sending message: %v", err)
 	}
 
 	return nil
